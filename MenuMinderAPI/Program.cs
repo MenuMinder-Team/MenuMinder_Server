c431516e07df124dcaf4b49bf92cc8e774960a3a
PhucHoangVan <93416646+PhucHoangVan@users.noreply.github.com>
2023-12-01 10:43:28 +0700

    Add config AutoMapper

--- MenuMinderAPI/Program.cs
+++ MenuMinderAPI/Program.cs
@@ -1,3 +1,12 @@
+using AutoMapper;
+using BusinessObjects.DataModels;
+using MenuMinderAPI.MiddleWares;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repositories;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Configure AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
