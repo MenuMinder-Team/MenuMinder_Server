bb7f0074a5540a79891e41163a32d8e06e538219
PHoangLe <lehoangphuc180502@gmail.com>
2023-12-12 08:46:59 +0700

    remove Password in ResultAccountDTO

--- BusinessObjects/DTO/AccountDTO/AccountDTO.cs
+++ BusinessObjects/DTO/AccountDTO/AccountDTO.cs
@@ -25,7 +25,6 @@ namespace BusinessObjects.DTO.AccountDTO
     {
         public Guid AccountId { get; set; }
         public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string Role { get; set; }
         public DateTime? DateOfBirth { get; set; }
