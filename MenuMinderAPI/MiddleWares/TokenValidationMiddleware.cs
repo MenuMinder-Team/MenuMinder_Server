2cca7841148092a048295682793d4b46186a32ec
PhucHoangVan <93416646+PhucHoangVan@users.noreply.github.com>
2023-12-13 12:00:10 +0700

    Create Middleware Extract Token payload

---
+++ MenuMinderAPI/MiddleWares/TokenValidationMiddleware.cs
@@ -0,0 +1,106 @@
+﻿using BusinessObjects.DataModels;
+using BusinessObjects.DTO.AuthDTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using Services.Exceptions;
+using Services.Helpers;
+using System.Formats.Asn1;
+using System.Net;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace MenuMinderAPI.MiddleWares
+{
+    public class TokenValidationMiddleware : IMiddleware
+    {
+        private readonly JwtServices _jwtService;
+
+        public TokenValidationMiddleware(JwtServices jwtService)
+        {
+            this._jwtService = jwtService;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // Check matching route
+            if (!IsRouteToBeHandled(context))
+            {
+                await next(context);
+                return;
+            }
+
+            // Extract the token from the request header
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token == null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                context.Response.ContentType = "application/json";
+                var problem = new ProblemDetails()
+                {
+                    Status = (int)HttpStatusCode.Unauthorized,
+                    Type = "Unauthorized",
+                    Detail = "Expected AccessToken in Header of request."
+                };
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(problem));
+                await next(context);
+                return;
+            }
+
+            // validate Token
+            ResultValidateTokenDto payload = new ResultValidateTokenDto();
+            try
+            {
+                payload = this._jwtService.VerifyToken(token);
+
+                // create list claims
+                var claims = new List<Claim>
+                {
+                    new Claim("AccountId", payload.AccountId),
+                    new Claim("Email", payload.Email),
+                    new Claim("Role", payload.Role)
+                };
+                // create claims identity
+                var claimsIdentity = new ClaimsIdentity(claims, "Bearer");
+                // create claims principle
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                context.User = claimsPrincipal;
+                await next(context);
+                return;
+            }
+            catch (Exception)
+            {
+                await next(context);
+            }
+        }
+
+        private bool IsRouteToBeHandled(HttpContext context)
+        {
+            // List path api want to apply middle ware
+            var targetRoutes = new Dictionary<string, string>
+            {
+                { "/api/auth/test-verify-token", "POST" },
+                { "/api/accounts/create", "POST" },
+            };
+            // get information of request
+            var requestPath = context.Request.Path;
+            var requestMethod = context.Request.Method;
+
+            // check paths is matching
+            if (targetRoutes.Any(route =>
+                requestPath.StartsWithSegments(route.Key) &&
+                string.Equals(requestMethod, route.Value, StringComparison.OrdinalIgnoreCase)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
